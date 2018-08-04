c274a22af1cfb3544dd908948a5147cc2e0d58d0
Carina Cruz <lecruz@bu.edu>
2018-08-04 17:09:51 +0000

    first commit

---
+++ string_calculator.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string>
+
+// The essence is a method that given a delimited string, returns the sum of the values.
+
+// An empty string returns zero
+
+if (str[0] == '\0')
+{
+    return 0;
+   }
+
+// A single number returns the value
+if (str[1] == '1')
+{
+    return 1;
+   }
+
+// Two numbers, comma delimited, returns the sum
+
+
+
+// Two numbers, newline delimited, returns the sum
+
+// Three numbers, delimited either way, returns the sum
+
+// Negative numbers throw an exception
+
+// Numbers greater than 1000 are ignored
+
+// A single char delimiter can be defined on the first line (e.g. //# for a ‘#’ as the delimiter)
+
+// A multi char delimiter can be defined on the first line (e.g. //[###] for ‘###’ as the delimiter)
+
+// Many single or multi-char delimiters can be defined (each wrapped in square brackets)
+
